c4369a475b99fa95062e6d8a9ff10ab00565ddfb
Alexi Collahuazo <a.ricardocollahuazo@gmail.com>
2024-12-13 17:09:56 -0500

    n

--- conexion.php
+++ conexion.php
@@ -4,10 +4,9 @@
     $clave = "";
     $nombre_bd = "sistema_alojamiento";
 
-// Crear la conexión
+
 $conexion = new mysqli($ip, $usuario, $clave, $nombre_bd);
 
-// Verificar la conexión
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
