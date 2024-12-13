2d684d81b5d462809d60bf35c0be1716a47fef15
Alexi Collahuazo <a.ricardocollahuazo@gmail.com>
2024-12-13 18:14:27 -0500

    MailTrap

--- vistas/plataformas/usuarios/procesar_reserva.php
+++ vistas/plataformas/usuarios/procesar_reserva.php
@@ -1,5 +1,10 @@
 <?php
+
 include_once '../../../conexion.php';
+require '/vendor/autoload.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id_espacio = $_POST['id_espacio'];
@@ -14,10 +19,50 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if (mysqli_query($conexion, $query)) {
         echo "Reserva realizada con éxito";
-    } else {
 
+        $id_reserva = mysqli_insert_id($conexion);
+        $query_usuario = "SELECT correo FROM usuarios WHERE id_usuario = '$id_usuario'";
+        $result = mysqli_query($conexion, $query_usuario);
+        $row = mysqli_fetch_assoc($result);
+        $email = $row['email'];
+
+
+        enviarCorreoConDetalles($email, $id_reserva, $id_espacio, $fecha_inicio, $fecha_fin);
+
+
+    } else {
         echo "Error al realizar la reserva: " . mysqli_error($conexion);
     }
     mysqli_close($conexion);
+}
+
+
+function enviarCorreoConDetalles($email, $id_reserva, $id_espacio, $fecha_inicio, $fecha_fin) {
+    $mail = new PHPMailer(true);
+
+    try {
+  
+        $mail->isSMTP();                                           
+        $mail->Host       = 'sandbox.smtp.mailtrap.io';                       
+        $mail->SMTPAuth   = true;                                    
+        $mail->Username   = '03d058af084204';                          
+        $mail->Password   = '3829c0bb94e4b1';                          
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         
+        $mail->Port       = 587;                                   
+
+        $mail->setFrom('no-reply@tusitio.com', 'Sistema de Reservas');
+        $mail->addAddress($email);                                 
+
+        $mail->Subject = "Confirmación de Reserva";
+        $mail->Body    = "¡Gracias por tu reserva! Aquí están los detalles de tu reserva:\n\n" .
+                         "ID de Reserva: $id_reserva\n" .
+                         "Espacio Reservado: $id_espacio\n" .
+                         "Fecha de Inicio: $fecha_inicio\n" .
+                         "Fecha de Fin: $fecha_fin\n\n" ;
+        $mail->send();
+        echo 'Correo enviado';
+    } catch (Exception $e) {
+        echo "Error al enviar el correo: {$mail->ErrorInfo}";
+    }
 }
 ?>
